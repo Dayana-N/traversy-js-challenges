48734a547bd11cd02efbcaf8c19d46beffa4b6e8
Dayana <dayana.nashkova@yahoo.com>
2023-09-16 23:29:30 +0000

    solve 06 title case

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,10 @@
-function titleCase() {}
+function titleCase(str) {
+  strArr = str.toLowerCase().split(' ');
+  for (let i = 0; i < strArr.length; i++) {
+    strArr[i] = strArr[i][0].toUpperCase() + strArr[i].slice(1);
+  }
+  let result = strArr.join(' ');
+  return result;
+}
 
 module.exports = titleCase;
