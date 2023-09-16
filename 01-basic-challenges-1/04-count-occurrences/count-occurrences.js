0fcb97f1afaf3d5fb60a63aad85ef4fcf79ef052
Dayana <dayana.nashkova@yahoo.com>
2023-09-16 11:53:37 +0000

    solve 04 count occurrences

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,13 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  let num = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      num++;
+    } else {
+      continue;
+    }
+  }
+  return num;
+}
 
 module.exports = countOccurrences;
