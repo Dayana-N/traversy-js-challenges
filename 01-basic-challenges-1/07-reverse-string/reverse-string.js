ff52ac6225911569646d38e4e203c86caa141547
Dayana <dayana.nashkova@yahoo.com>
2023-09-22 15:22:47 +0000

    complete 7 reverse string

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,9 @@
-function reverseString() {}
+function reverseString(str) {
+  reverse = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  }
+  return reverse;
+}
 
 module.exports = reverseString;
